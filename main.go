9fdb8600cb5b2eede5be452861da5a677bef54d5
Athioush Ranjan <athioushranjan@gmail.com>
2022-01-24 02:03:31 +0530

    add git ignore file

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 )
 
 func main() {
+	// add print handler to the list
 	http.HandleFunc("/docker", printHandler)
+	// listen and serve to port 8080
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		logrus.Fatalf("unable to listen the server at the port %s", "8080")
 	}
@@ -17,9 +19,12 @@ func main() {
 }
 
 func printHandler(w http.ResponseWriter, r *http.Request) {
+	// print message
 	_, err := fmt.Fprintf(w, "learn docker")
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Fatalf("can't print data %v", err)
+		return
 	}
 	w.WriteHeader(http.StatusOK)
 }
