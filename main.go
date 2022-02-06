c39f30d18ad6232bd7b706ada3fff427880c1e41
Athioush Ranjan <athioushranjan@gmail.com>
2022-02-06 20:45:26 +0530

    changes in main file

--- main.go
+++ main.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/athioushranjan/docker-learn/database"
 	"github.com/sirupsen/logrus"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -13,21 +11,21 @@ func main() {
 	http.HandleFunc("/docker", printHandler)
 	// load env
 	//database.LoadEnv()
-	logrus.Println(os.Getenv("DB_HOST"))
-	logrus.Println(os.Getenv("DB_PORT"))
-	logrus.Println(os.Getenv("DB_NAME"))
-	logrus.Println(os.Getenv("DB_USER"))
-	logrus.Println(os.Getenv("DB_PASS"))
-	// connect database and migrate
-	if err := database.ConnectAndMigrate(os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		database.SSLModeDisable); err != nil {
-		logrus.Panicf("Failed to initialize and migrate database with error: %+v", err)
-	}
-	logrus.Print("database connection and migration successful!!")
+	//logrus.Println(os.Getenv("DB_HOST"))
+	//logrus.Println(os.Getenv("DB_PORT"))
+	//logrus.Println(os.Getenv("DB_NAME"))
+	//logrus.Println(os.Getenv("DB_USER"))
+	//logrus.Println(os.Getenv("DB_PASS"))
+	//// connect database and migrate
+	//if err := database.ConnectAndMigrate(os.Getenv("DB_HOST"),
+	//	os.Getenv("DB_PORT"),
+	//	os.Getenv("DB_NAME"),
+	//	os.Getenv("DB_USER"),
+	//	os.Getenv("DB_PASS"),
+	//	database.SSLModeDisable); err != nil {
+	//	logrus.Panicf("Failed to initialize and migrate database with error: %+v", err)
+	//}
+	//logrus.Print("database connection and migration successful!!")
 	// listen and serve to port 8080
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		logrus.Fatalf("unable to listen the server at the port %s", "8080")
